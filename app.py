6392686d4459a566034840d2c2c67ecca5f88c58
Selina Tepe <32930832+SelinaTepe@users.noreply.github.com>
2025-01-15 02:27:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Fallback auf 5000, falls PORT nicht gesetzt
     app.run(host="0.0.0.0", port=port)
 
 
