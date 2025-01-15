4175946c66b0a4c0219d1b4727ecf777b8ef0d27
Selina Tepe <32930832+SelinaTepe@users.noreply.github.com>
2025-01-15 02:17:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render stellt den Port über die Umgebungsvariable PORT bereit
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 # Datenstruktur für Empfehlungen
